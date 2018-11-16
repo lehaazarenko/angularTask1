f5922e5943e0463a54d1724df24798963d6b1d5d
Aliaksei Azaranka <aliaksei_azaranka@epam.com>
2018-11-16 11:09:44 +0300

    debug-branch: just for debugging

--- myCtrl.js
+++ myCtrl.js
@@ -1,6 +1,10 @@
-var app = angular.module("angularTask1", []); 
+const app = angular.module("angularTask1", []); 
 app.controller("myCtrl", function($scope) {
     $scope.comments = [{text: "Nice...", isRemoved: false}, 
-    				   {text:"Dumb", isRemoved: false}, 
-    				   {text: "Dope", isRemoved: false}];
+    				   {text:"Dumb", isRemoved: true}, 
+    				   {text: "Dope", isRemoved: false},
+    				   {text: "Brilliant", isRemoved: false},
+    				   {text: "Generic", isRemoved: false},
+    				   {text: "Outstanding", isRemoved: true},
+    				   {text: "Borring", isRemoved: false}];
 });
