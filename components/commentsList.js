9667ca5e6183aaf89b0c289d789a6e272af1f034
Katsiaryna.Sachko <Katsiaryna_Sachko@epam.com>
2018-11-18 19:35:50 +0300

    correct

--- components/commentsList.js
+++ components/commentsList.js
@@ -1,29 +1,55 @@
-(function(angular) {
-	'use strict';
-	function commentsListController($scope, $element, $attrs) {
-		const ctrl = this;
+(function (angular) {
+    'use strict';
 
-		ctrl.comments = [{text: "Nice...", isRemoved: false}, 
-	    				   {text:"Dumb", isRemoved: false},
-	    				   {text: "Dope", isRemoved: false},
-	    				   {text: "Brilliant", isRemoved: false},
-	    				   {text: "Generic", isRemoved: false},
-	    				   {text: "Outstanding", isRemoved: false},
-	    				   {text: "Borring", isRemoved: false}];
+    function commentsListController() {
+        const ctrl = this;
 
-        ctrl.toggleIsRemoved = function (index) {
+        ctrl.comments = [
+            {text: "Nice...", isRemoved: false},
+            {text: "Dumb", isRemoved: false},
+            {text: "Dope", isRemoved: false},
+            {text: "Brilliant", isRemoved: false},
+            {text: "Generic", isRemoved: false},
+            {text: "Outstanding", isRemoved: false},
+            {text: "Borring", isRemoved: false}
+        ];
+        ctrl.commentsTypes = [
+            {value: 'all', label: 'All'},
+            {value: 'existing', label: 'Existing'},
+            {value: 'archived', label: 'Archived'}
+        ];
+        ctrl.selectedType = 'all';
+        ctrl.commentsList = ctrl.comments;
+
+
+        ctrl.toggleIsRemoved = (index) => {
             ctrl.comments[index].isRemoved === true ?
-				ctrl.comments[index].isRemoved = false :
-				ctrl.comments[index].isRemoved = true;
-			// console.log(index);
-        }
-	}
+                ctrl.comments[index].isRemoved = false :
+                ctrl.comments[index].isRemoved = true;
+            // console.log(index);
+        };
+
+        ctrl.updateCommentsList = () => {
+            switch (ctrl.selectedType) {
+                case 'all':
+                    ctrl.commentsList = ctrl.comments;
+                    break;
+                case 'existing':
+                    ctrl.commentsList = ctrl.comments.filter(item => item.isRemoved === false);
+                    break;
+                case 'archived':
+                    ctrl.commentsList = ctrl.comments.filter(item => item.isRemoved === true);
+                    break;
+                default:
+                    ctrl.commentsList = ctrl.comments;
+            }
+        };
+    }
 
 
-
-	angular.module('angularTask1').component('commentsList', {
-		templateUrl: 'components/commentsListView.html',
-		controller: commentsListController,
-		controllerAs: 'ctrl'
-	});
+    angular.module('angularTask1').component('commentsList', {
+        templateUrl: 'components/commentsListView.html',
+        controller: commentsListController,
+        controllerAs: 'ctrl'
+    });
 })(window.angular);
