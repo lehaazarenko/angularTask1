ca9d61d5aa0398574f15d8ea71a72ab72e9f2e70
User <lehaazarenko@mail.ru>
2018-11-18 18:14:46 +0300

    experiments: task 1 final (???)

--- components/commentsList.js
+++ components/commentsList.js
@@ -5,21 +5,25 @@
 
 		ctrl.comments = [{text: "Nice...", isRemoved: false}, 
 	    				   {text:"Dumb", isRemoved: false},
-	    				   {text: "Dope", isRemoved: false},
+	    				   {text: "Dope", isRemoved: true},
 	    				   {text: "Brilliant", isRemoved: false},
-	    				   {text: "Generic", isRemoved: false},
+	    				   {text: "Generic", isRemoved: true},
 	    				   {text: "Outstanding", isRemoved: false},
 	    				   {text: "Borring", isRemoved: false}];
+
+	    ctrl.areDisplayedCommentsArchived = false; 
+	    // Added due to not loading archived checkbox checked property on load
+
+	    ctrl.toggleIsArchived = function() {
+	    	ctrl.areDisplayedCommentsArchived = !ctrl.areDisplayedCommentsArchived;
+	    } 
 
         ctrl.toggleIsRemoved = function (index) {
             ctrl.comments[index].isRemoved === true ?
 				ctrl.comments[index].isRemoved = false :
 				ctrl.comments[index].isRemoved = true;
-			// console.log(index);
         }
 	}
-
-
 
 	angular.module('angularTask1').component('commentsList', {
 		templateUrl: 'components/commentsListView.html',
