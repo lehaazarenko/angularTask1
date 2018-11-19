62fb317dee0e852fc167cbc7d9bd75caecc7186b
Aliaksei Azaranka <aliaksei_azaranka@epam.com>
2018-11-19 15:06:48 +0300

    experiments: task 2 without classes, service and localStorage

--- components/commentsList.js
+++ components/commentsList.js
@@ -1,29 +1,91 @@
 (function(angular) {
 	'use strict';
-	function commentsListController($scope, $element, $attrs) {
+	function commentsListController() {
 		const ctrl = this;
 
-		ctrl.comments = [{text: "Nice...", isRemoved: false}, 
-	    				   {text:"Dumb", isRemoved: false},
-	    				   {text: "Dope", isRemoved: true},
-	    				   {text: "Brilliant", isRemoved: false},
-	    				   {text: "Generic", isRemoved: true},
-	    				   {text: "Outstanding", isRemoved: false},
-	    				   {text: "Borring", isRemoved: false}];
+		class Comment {
+			constructor(text, username, date, isRemoved) {
+				this.text = text;
+				this.username = username;
+				this.creationDate = date;
+				this.editDate = '';
+				this.isRemoved = isRemoved;
+				this.isEdited = false;
+			}
+		}
 
-	    ctrl.areDisplayedCommentsArchived = false; 
+		ctrl.newDate = new Date();
+
+		ctrl.comments = [{text: "Nice...", username: "Vera",
+						  creationDate: new Date(), editDate: '',
+						  isRemoved: false, isEdited: false},
+					     {text:"Dumb", username: "Elena",
+						  creationDate: new Date(), editDate: new Date(),
+						  isRemoved: false, isEdited: true},
+					     {text: "Dope", username: "Alexandr",
+						  creationDate: new Date(), editDate: '',
+						  isRemoved: true, isEdited: false},
+					     {text: "Brilliant", username: "Alexei",
+						  creationDate: new Date(), editDate: '',
+						  isRemoved: false, isEdited: false},
+					     {text: "Generic", username: "Anthony",
+						  creationDate: new Date(), editDate: '',
+						  isRemoved: true, isEdited: false},
+					     {text: "Outstanding", username: "Jack",
+						  creationDate: new Date(), editDate: '',
+						  isRemoved: false, isEdited: false},
+					     {text: "Borring", username: "Eva",
+						  creationDate: new Date(), editDate: '',
+						  isRemoved: false, isEdited: false}];
+
+	    ctrl.areDisplayedCommentsArchived = false;
+	    ctrl.isEditPopupDisplayed = false;
+	    ctrl.commentForEditing = {};
 
 	    ctrl.getComments = () => {
-	    	return ctrl.areDisplayedCommentsArchived ? ctrl.comments.filter(x => x.isRemoved) : ctrl.comments;
-	    }
+	    	return ctrl.areDisplayedCommentsArchived ?
+				ctrl.comments.filter(x => x.isRemoved) :
+				ctrl.comments.filter(x => !x.isRemoved);
+	    };
 
 	    ctrl.toggleIsArchived = () => {
 	    	ctrl.areDisplayedCommentsArchived = !ctrl.areDisplayedCommentsArchived;
-	    } 
+	    };
 
         ctrl.toggleIsRemoved = (comment) => {
         	const index = ctrl.comments.indexOf(comment);
 			ctrl.comments[index].isRemoved = !ctrl.comments[index].isRemoved;
+        };
+
+        ctrl.addComment = (username, comment) => {
+        	const newComment = new Comment(comment, username, false);
+        	ctrl.comments.push(newComment);
+		};
+
+        ctrl.editComment = (comment) => {
+        	const index = ctrl.comments.indexOf(comment);
+        	ctrl.commentForEditing = ctrl.comments[index];
+        	ctrl.isEditPopupDisplayed = true;
+        	console.log(ctrl.commentForEditing);
+
+            ctrl.editCommentConfirm = (username, comment) => {
+				updateComment(index, username, comment);
+				ctrl.isEditPopupDisplayed = false;
+            }
+
+		};
+
+        ctrl.editCommentCancel = () => {
+        	ctrl.isEditPopupDisplayed = false;
+		};
+
+        const updateComment = (index, username, comment) => {
+            ctrl.comments[index].text = comment === undefined ?
+                ctrl.comments[index].text : comment;
+            ctrl.comments[index].username = username === undefined ?
+                ctrl.comments[index].username : username;
+            ctrl.comments[index].isEdited = true;
+            ctrl.comments[index].editDate = new Date();
         }
 	}
 
